9ee7c642d81ffede3a2846847b018161e8a464d0
YunfeiGu <gyf6968@hotmail.com>
2017-03-26 02:32:01 -0500

    add test bench

---
+++ CNN_tb.v
@@ -0,0 +1,12 @@
+/*****************************************************************************************************
+ * Description:                 Test bench for CNN hardware accelerator
+ *                              -- This test bench mainly used for test the functionality of CNN 
+ *
+ * Author:                      Yunfei Gu
+ *
+ * Email:                       gu.yunfei@wustl.edu
+ *
+ * Rev History:
+ *       <Author>        <Date>        <Hardware>     <Version>        <Description> 
+ *     Yunfei Gu       2017-03-25          --           1.00             Create
+ *****************************************************************************************************/
