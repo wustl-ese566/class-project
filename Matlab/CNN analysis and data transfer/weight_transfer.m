bc6be74233de833d1c02f8f1684e99346fc69861
YunfeiGu <gyf6968@hotmail.com>
2017-05-03 16:15:43 -0500

    fixed 16 bit fixed point data issue

--- Matlab/CNN analysis and data transfer/weight_transfer.m
+++ Matlab/CNN analysis and data transfer/weight_transfer.m
@@ -6,8 +6,11 @@ imgdata=fread(fl,'float');
 fclose(fl);
     fff=fopen('weight.dat', 'wb');
      a=quantizer([16 12]);
-     a=num2bin(a,imgdata);
+     all=num2bin(a,imgdata);
      %a=num2str(a);
-     fwrite(fff,a);
+     %fwrite(fff,a);
+     at=num2int(a,imgdata);
+     fwrite(fff,at,'integer*2');
+     
      fclose(fff);
 
